0b206db06774d35f27ed37d4164d1471db364682
Carlos Opena <2224137@slu.edu.ph>
2026-03-29 03:15:31 +0800

    Fixed Hamburger menu and sidebar menu

--- carlos-Dgreat/src/components/Navbar.jsx
+++ carlos-Dgreat/src/components/Navbar.jsx
@@ -94,10 +94,10 @@ export default function Navbar() {
             >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                className="h-6 w-6 text-black"
+                className="h-6 w-6"
                 fill="none"
                 viewBox="0 0 24 24"
-                stroke="currentColor"
+                stroke={isScrolled ? '#ffffff' : '#000000'}
               >
                 <path
                   strokeLinecap="round"
